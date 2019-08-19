27d236f88d195a6ad0fb4851b581a1ae0d747850
Pejo Linchangco <peter.linchangco@afpayments.com>
2019-08-19 15:41:47 +0800

    Making buildable project

--- app/src/main/java/afpayments/beepdirectoryproject/MainActivity.kt
+++ app/src/main/java/afpayments/beepdirectoryproject/MainActivity.kt
@@ -1,7 +1,5 @@
 package afpayments.beepdirectoryproject
 
-import afpayments.beepStorage.BeepStorage
-import afpayments.beepStorage.fileType
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -13,13 +11,5 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        Log.e("BeepStorage", BeepStorage.getNonVolatileStoragePath(fileType.logs))
-
-        val myfile = BeepStorage.getNonVolatileStorage(fileType.logs,"test.txt")
-
-        myfile.writeText("Today is tuesday")
-
     }
 }
